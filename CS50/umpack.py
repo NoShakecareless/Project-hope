8b11a357e83463b5588a57c846af3b1c4c240370
MyCodingWay <1848142281@qq.com>
2023-08-16 13:19:11 +0800

    parse pack

--- CS50/umpack.py
+++ CS50/umpack.py
@@ -1,3 +1,16 @@
+import argparse
+import sys
+
+
+def parse_args():
+    parse = argparse.ArgumentParser(description="Unpacking learn")
+    parse.add_argument('galleons', type=int, help='number of galleons')
+    parse.add_argument('sickles', type=int, help='number of sickles')
+    parse.add_argument('knuts', type=int, help='number of knuts')
+    args = parse.parse_args()   # 解析参数对象获得解析对象
+    return args
+
+
 def total(galleons, sickles, knuts):
     return (galleons*17 + sickles) * 29 + knuts
 
@@ -12,3 +25,12 @@ unpacking
     total(coins) can not pass
     but with '*' pass!
 """
+
+if __name__ == "__main__":
+    # args = parse_args()
+    if len(sys.argv) < 4:
+        raise ValueError("Plz input 3 elem!")
+    galleons = int(sys.argv[1])
+    sickles = int(sys.argv[2])
+    knuts = int(sys.argv[3])
+    print(total(galleons, sickles, knuts), "Knuts")
