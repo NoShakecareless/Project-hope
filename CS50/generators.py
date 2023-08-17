d3c43a3a62ffae509978709a519d8425037f04a5
NoshakeCareless <1848142281@qq.com>
2023-08-17 16:34:05 +0800

    generator end

---
+++ CS50/generators.py
@@ -0,0 +1,19 @@
+"""
+Example for generator
+sleep.py
+"""
+
+
+def main():
+    n = int(input("What's n? "))
+    for s in sheep(n):
+        print(s)
+
+
+def sheep(n):
+    for i in range(n):
+        yield "🐏" * i
+
+
+if __name__ == "__main__":
+    main()
